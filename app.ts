7f66ea777db233b8839a4b879e0cebfbdef85ae5
singhrobinn <44369743+singhrobinn@users.noreply.github.com>
2022-04-16 03:06:48 +0530

    Generic tutorials

--- app.ts
+++ app.ts
@@ -1,1 +1,88 @@
-console.log("YO")
+
+//GENERIC EXAMPLE 
+
+function merge<T extends object, U extends object>(objA: T, objB:U){
+    return Object.assign(objA, objB);
+}
+
+const merged = merge({name: 'Robin'}, {age:25});
+
+console.log(merged.name);
+
+interface lengthy{
+    length : number;
+}
+
+
+function countAndPrint<T extends lengthy>(objA: T):[T, string, number]{
+    let descriptionText: string;
+    if(objA.length===0){
+        descriptionText = '0 length text';
+    }
+    else if(objA.length===1){
+        descriptionText = '1 length text';
+    }
+    else{
+        descriptionText = 'long ass text';
+    }
+    return [objA, descriptionText, objA.length];
+}
+
+
+console.log(countAndPrint('BIIGGG'));
+
+
+
+
+
+
+
+
+//GENERIC CLASSES
+
+class DataStorage<T>{
+    private data: T[] = [];
+
+    addItem(item: T){
+        this.data.push(item);
+    }
+
+    removeItem(item: T){
+        if(this.data.indexOf(item)===-1){
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems(){
+        return [...this.data];
+    }
+
+}
+
+const dataValue = new DataStorage<object>();
+const objToBeDeleted = {name:'SOME NAMEE'};
+dataValue.addItem({name:'SOME NAMEE'});
+dataValue.addItem(objToBeDeleted);
+dataValue.removeItem(objToBeDeleted);
+console.log(dataValue.getItems());
+
+
+
+
+//GENERIC UTILITY TYPES
+
+interface CourseGoal{
+    title: string;
+    description: string;
+    completionDate : Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal{
+    let goal: Partial<CourseGoal> = {};
+    goal.completionDate=date;
+    goal.description=description;
+    goal.title=title;
+    return goal as CourseGoal;
+}
+
